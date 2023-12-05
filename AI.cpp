8147abffe78f36a1645cf7e6ba5190afa76265b2
bridgell <150395631+bridgell@users.noreply.github.com>
2023-12-04 22:54:08 -0500

    Update AI.cpp

--- AI.cpp
+++ AI.cpp
@@ -22,5 +22,76 @@ string getAIMoveString(const BuildingState& buildingState) {
 
 string getAIPickupList(const Move& move, const BuildingState& buildingState, 
                        const Floor& floorToPickup) {
-    return "";
+    string pickupList = "";
+    int upRequests = 0;
+    int downRequests = 0;
+    int angerLevelUp = 0;
+    int angerLevelDown = 0;
+
+    
+    //comparing angerlevels of up and down requests and adding to pickup list
+    int currentNumPeople = floorToPickup.getNumPeople();
+    for (int p = 0; p < currentNumPeople; p++) {
+        Person currentPerson = floorToPickup.getPersonByIndex(p);
+        int targetFloor = currentPerson.getTargetFloor();
+        int currentFloor = currentPerson.getCurrentFloor();
+        if (currentFloor < targetFloor) {
+            upRequests++;
+        } else {
+            downRequests++;
+        }
+    }
+    //if up requests and down requests only differ by one person consider anger levels
+    if (abs(upRequests-downRequests) <= 1) {
+        for (int i = 0; i < currentNumPeople; i++) {
+            Person currentPerson = floorToPickup.getPersonByIndex(i);
+            int targetFloor = currentPerson.getTargetFloor();
+            int currentFloor = currentPerson.getCurrentFloor();
+            if (currentFloor < targetFloor) {
+                angerLevelUp += currentPerson.getAngerLevel();
+            } else {
+                angerLevelDown += currentPerson.getAngerLevel();
+            }
+        }
+        if (angerLevelUp > angerLevelDown) {
+            for (int j = 0; j < currentNumPeople; j++) {
+                Person currentPerson = floorToPickup.getPersonByIndex(j);
+                int targetFloor = currentPerson.getTargetFloor();
+                int currentFloor = currentPerson.getCurrentFloor();
+                if (currentFloor < targetFloor) {
+                    pickupList += j;
+                }
+            }
+        } else {
+            for (int k = 0; k < currentNumPeople; k++) {
+                Person currentPerson = floorToPickup.getPersonByIndex(k);
+                int targetFloor = currentPerson.getTargetFloor();
+                int currentFloor = currentPerson.getCurrentFloor();
+                if (currentFloor > targetFloor) {
+                    pickupList += k;
+                }
+            }
+        }
+    } else if (abs(upRequests-downRequests) >= 2) {
+        if (upRequests > downRequests) {
+            for (int p = 0; p < currentNumPeople; p++) {
+                Person currentPerson = floorToPickup.getPersonByIndex(p);
+                int targetFloor = currentPerson.getTargetFloor();
+                int currentFloor = currentPerson.getCurrentFloor();
+                if (currentFloor < targetFloor) {
+                    pickupList += p;
+                }
+            }
+        } else {
+            for (int c = 0; c < currentNumPeople; c++) {
+                Person currentPerson = floorToPickup.getPersonByIndex(c);
+                int targetFloor = currentPerson.getTargetFloor();
+                int currentFloor = currentPerson.getCurrentFloor();
+                if (currentFloor > targetFloor) {
+                    pickupList += c;
+                }
+            }
+        }
+    }
+    return pickupList;
 }
